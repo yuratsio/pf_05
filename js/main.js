b32bb1bf481afea57917c1511e37833c09ef1dd5
yuratsio <93855185+yuratsio@users.noreply.github.com>
2022-02-05 13:23:19 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,127 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+  
+  // =============================
+  // common
+  // =============================
+  const body = document.querySelector('body');
+  
+  // =============================
+  // hamburger
+  // =============================
+  const hamburgerButton = document.querySelector('.header__hamburger');
+  const hamburgerButtonLine = document.querySelector('.header__hamburger-line');
+  const headerNav = document.querySelector('.header__nav');
+  const headerMask = document.querySelector('.header__mask');
+  hamburgerButton.addEventListener('click', () => {
+    headerNav.classList.toggle('header__nav--open');
+    if(headerNav.classList.contains('header__nav--open')) {
+      hamburgerButtonLine.classList.add('header__hamburger-line--active');
+      headerMask.classList.add('header__mask--open');
+      body.style.overflowY = 'hidden';
+      return;
+    }
+    closeHamburger()
+  });
+  headerMask.addEventListener('click', () => {
+    closeHamburger()
+  });
+  
+  function closeHamburger() {
+    body.style.overflowY = 'auto';
+    hamburgerButtonLine.classList.remove('header__hamburger-line--active');
+    headerNav.classList.remove('header__nav--open');
+    headerMask.classList.remove('header__mask--open');
+  }
+  
+  // =============================
+  // page top
+  // =============================
+  const pageTop = document.querySelector('.page-top');
+  window.addEventListener('scroll', () => {
+    let topY = window.pageYOffset;
+    if(topY <= 0) {
+      pageTop.classList.remove('active');
+    } else {
+      pageTop.classList.add('active');
+    }
+  });
+
+  pageTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    const windowTop = 0;
+    window.scroll({
+      top: windowTop,
+      behavior: "smooth",
+    });
+  });
+  // =============================
+  // fade-in
+  // =============================
+  const fadeInTargets = document.querySelectorAll('.js-fade-in');
+  const fadeInOptions = {
+    threshold: .2,
+  }
+  const fadeInObs = new IntersectionObserver(fadeIn, fadeInOptions);
+
+  fadeInTargets.forEach(fadeInTarget => {
+    fadeInObs.observe(fadeInTarget);
+  });
+
+  function fadeIn(entries, obs) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+        return;
+      }
+      entry.target.classList.add('js-fade-in--active');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  // =============================
+  // animation
+  // =============================
+  const animationTargets = document.querySelectorAll('.animation');
+  const animationOptions = {
+    threshold: .3
+  };
+  const animationObs = new IntersectionObserver(animation, animationOptions);
+
+  animationTargets.forEach(animationTarget => {
+    animationObs.observe(animationTarget);
+  });
+
+  function animation(entries, obs) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+        return;
+      }
+      entry.target.classList.add('animation--active');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  // =============================
+  // resize
+  // =============================
+  const reloadBreakPoint = 767;
+  let resizeFlag;
+  if(reloadBreakPoint < window.innerWidth) {
+    resizeFlag = false;
+  } else {
+    resizeFlag = true;
+  };
+  resizeWindow();
+
+  function resizeWindow() {
+    window.addEventListener('resize', () => {
+      if(reloadBreakPoint < window.innerWidth && resizeFlag) {
+        window.location.reload();
+        resizeFlag = false;
+      } else if(reloadBreakPoint >= window.innerWidth && !(resizeFlag)) {
+        resizeFlag = true;
+      };
+    });
+  };
+});
